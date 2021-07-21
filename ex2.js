6dee5018b8f702867a49a83534187597222cf718
hasan <hasan@gmail.com>
2021-07-21 16:08:23 +0530

    ex2vue.html

--- ex2.js
+++ ex2.js
@@ -1,7 +1,19 @@
 // let a='Process Drive';
 // console.log(`Hello ${a}`);
 
-const apps=Vue.createApp({
-    template:`<h2>I m from Vue Js</h2>`
-});
-apps.mount("#app")
+// const apps=Vue.createApp({
+//     template:`<h2>I m from Vue Js</h2><h1>My Sample code on Vue js with CDN</h1>`
+// });
+// apps.mount("#app")
+var vmObj={name:"ProcessDrive"}
+Object.freeze(vmObj)
+var vm=new Vue({
+    el:'#app',
+    data:{
+        name: vmObj
+    }
+})
+
+console.log(vm.name.name)
+vmObj.name="Tirupattur"
+console.log(vm.name.name);
