725a5b69148fbb5f3801993b59d9eb26fee40b10
hasan <hasan@gmail.com>
2021-07-21 18:03:06 +0530

    ex2vue.html

--- ex2.js
+++ ex2.js
@@ -5,15 +5,30 @@
 //     template:`<h2>I m from Vue Js</h2><h1>My Sample code on Vue js with CDN</h1>`
 // });
 // apps.mount("#app")
-var vmObj={name:"ProcessDrive"}
-Object.freeze(vmObj)
-var vm=new Vue({
-    el:'#app',
+
+// var vmObj={name:"ProcessDrive"}
+// Object.freeze(vmObj)
+// var vm=new Vue({
+//     el:'#app',
+//     data:{
+//         //name: vmObj
+//         name:"ProcessDrive"
+//     },
+//     methods:{
+//         greet: function(){
+//         return `Good Evening ${this.name}`
+//      }
+//      }
+// })
+// console.log(vm.name.name)
+// vmObj.name="Tirupattur"
+// console.log(vm.name.name);
+var app=new Vue({
+    el: '#app',
     data:{
-        name: vmObj
+        product: 'Books',
+        image:'images/book-image.jpg',
+        inventory:12,
+        details:['Javascript','C++','Python','PHP']
     }
 })
-
-console.log(vm.name.name)
-vmObj.name="Tirupattur"
-console.log(vm.name.name);
