f71b882d75118819cd67235ac145c7a391c24792
ProcessDrive <email@processdrive.com>
2021-07-23 10:49:55 +0530

    modified

--- sample.js
+++ sample.js
@@ -1,2 +1,15 @@
-console.log('My first sample project on gitHub')
-console.log('This is Trial for modification')
+// console.log('My first sample project on gitHub')
+// console.log('This is Trial for modification')
+var data={a:1}
+var vueInst = new Vue({
+    el:'#simple',
+    data:{
+        data:data
+    }
+})
+console.log(vueInst.data.a)
+vueInst.data='change';
+console.log(vueInst.data)
+vueInst.data='changedValue'
+data.a='changedValue'
+console.log(data.a);
